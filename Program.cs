ca27cfea31381d70a569c4fda07057abb803081c
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-02-01 15:55:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; //C# 6 ile gelen özellik
+using static System.ConsoleColor;
+using StudentRepository.Context;
+using StudentRepository.Models;
+
+namespace LinqToXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
